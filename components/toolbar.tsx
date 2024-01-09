5e80e61fcf7af8f883500b3b110a76560c5dae25
TreNgheDiCode <gabayan170@gmail.com>
2024-01-09 18:52:11 +0700

    Toolbar

---
+++ components/toolbar.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import { Doc } from "@/convex/_generated/dataModel";
+import { IconPicker } from "./icon-picker";
+import { Button } from "./ui/button";
+import { ImageIcon, Smile, X } from "lucide-react";
+import { ElementRef, KeyboardEvent, useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import TextareaAutosize from "react-textarea-autosize";
+
+interface ToolbarProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
+}
+
+export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditting, setIsEdtting] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+  const removeIcon = useMutation(api.documents.removeIcon);
+
+  const enableInput = () => {
+    if (preview) return;
+
+    setIsEdtting(true);
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
+    }, 0);
+  };
+
+  const disableInput = () => setIsEdtting(false);
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
+
+  const onIconSelect = (icon: string) => {
+    update({
+      id: initialData._id,
+      icon,
+    });
+  };
+
+  const onRemoveIcon = () => {
+    removeIcon({ id: initialData._id });
+  };
+  return (
+    <div className="group relative pl-[54px]">
+      {!!initialData.icon && !preview && (
+        <div className="group/icon flex items-center gap-x-2 pt-6">
+          <IconPicker onChange={onIconSelect}>
+            <p className="text-6xl transition hover:opacity-75">
+              {initialData.icon}
+            </p>
+          </IconPicker>
+          <Button
+            onClick={onRemoveIcon}
+            className="rounded-full text-xs text-muted-foreground opacity-0 transition group-hover/icon:opacity-100"
+            variant={"outline"}
+            size={"icon"}
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </div>
+      )}
+      {!!initialData.icon && preview && (
+        <p className="pt-6 text-6xl">{initialData.icon}</p>
+      )}
+      <div className="flex items-center gap-x-1 py-4 opacity-0 group-hover:opacity-100">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={onIconSelect}>
+            <Button
+              className="text-xs text-muted-foreground"
+              variant={"outline"}
+              size={"sm"}
+            >
+              <Smile className="mr-2 h-4 w-4" />
+              Add icon
+            </Button>
+          </IconPicker>
+        )}
+        {!initialData.coverImage && !preview && (
+          <Button
+            onClick={() => {}}
+            className="text-xs text-muted-foreground"
+            variant={"outline"}
+            size={"sm"}
+          >
+            <ImageIcon className="mr-2 h-4 w-4" />
+            Add cover
+          </Button>
+        )}
+      </div>
+      {isEditting && !preview ? (
+        <TextareaAutosize
+          ref={inputRef}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          value={value}
+          onChange={(e) => onInput(e.target.value)}
+          className="resize-none break-words bg-transparent text-5xl font-bold text-[#3F3F3F] outline-none dark:text-[#CFCFCF]"
+        />
+      ) : (
+        <div
+          onClick={enableInput}
+          className="ouline-none break-words pb-[11.5px] text-5xl font-bold text-[#3F3F3F] dark:text-[#CFCFCF]"
+        >
+          {initialData.title}
+        </div>
+      )}
+    </div>
+  );
+};
